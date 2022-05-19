988c620a9b34cc335ac6970557f8ac75fb3d3e6e
asrop223 <105834152+asrop223@users.noreply.github.com>
2022-05-19 10:06:29 +0700

    Add files via upload

---
+++ ukkrpl2022/header.php
@@ -0,0 +1,10 @@
+<h4 align="right"><a href="logout.php">Logout</a></h4>
+<table align="center" width="70%">
+	<tr>
+		<td width="150px"><img src="icon/icon.png" alt=""></td>
+		<td align="left"><h2>PEDULI DIRI</h2>
+		<p>Catatan Perjalanan</p>
+		<p><a href="home.php">HOME</a> | <a href="catatan.php">Catatan Perjalanan</a> | <a href="isidata.php">Isi Data</a> | </p>
+		</td>
+	</tr>
+</table>
---
+++ ukkrpl2022/home.php
@@ -0,0 +1,11 @@
+<?php
+include "header.php";
+session_start();
+?>
+<html>
+<body>
+	<table border="1" height="40%" width="50%" align="center">
+	<td> Selamat datang <?php echo $_SESSION['username'] ?> di aplikasi peduli diri</td>
+	</table>
+</body>
+</html>
